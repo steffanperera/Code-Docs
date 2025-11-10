c4ee915f2b9df4f2c9dfbb6a7b1ec3be1f4da9a8
steffanperera <steffanperera1997@gmail.com>
2025-11-10 22:49:10 +1100

    update

--- 01-For-Loop/02/02.py
+++ 01-For-Loop/02/02.py
@@ -4,4 +4,8 @@ def even_numbers(i = 1):
     output = []
     for i in range (1, 21):
         if i%2 == 0:
+            output.append(i)
+    return output
+
+print(even_numbers())
             
--- 01-For-Loop/03/03.js
+++ 01-For-Loop/03/03.js
@@ -1,16 +1,11 @@
 // Print the sum of numbers from 1 to 100
 
 const myFunction = (i = 1) => {
-  let numbers = [];
   let sum = 0;
 
   for (i; i <= 100; i++) {
-    numbers.push(i);
+    sum = sum + i;
   }
-
-  numbers.forEach((number) => {
-    sum = sum + number;
-  });
 
   return sum;
 };
